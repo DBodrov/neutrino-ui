31b49f092f9f0bd1dc847b863c3a2922e249ec59
D.Bodrov <dmbodrov@hotmail.com>
2020-04-02 01:38:49 +0300

    build with babel

--- scripts/build.js
+++ scripts/build.js
@@ -9,14 +9,16 @@ function buildLib() {
     const components = fse.readdirSync(resolveApp('src')).filter(folder => folder !== 'index.ts');
     console.log('Clear Lib dir');
     fse.emptyDirSync('lib');
-    console.log('Compile....')
-    execSync(`yarn compile`);
-    console.log('Done.')
+    console.log('Create typings....')
+    execSync(`npm run compile`);
+    console.log('Typings Done.')
 
-    // components.forEach(folder => {
-    //     console.log(`Compile ${folder} ...`)
-    //     // execSync(`npx babel ${resolveApp(`src/${folder}`)} --out-dir ${resolveApp(`lib/${folder}`)} --extensions .tsx,.ts --source-maps --ignore "src/**/*.spec.tsx","__tests__"`);
-    // })
+    components.forEach(folder => {
+        console.log(`Compile ${folder} ...`)
+        execSync(`npx babel ${resolveApp(`src/${folder}`)} --out-dir ${resolveApp(`lib/${folder}`)} --extensions .tsx,.ts --source-maps --ignore "src/**/*.spec.tsx","__tests__"`);
+    });
+    execSync(`npx babel ${resolveApp('src/index.ts')} --out-file ${resolveApp('lib/index.js')}`);
+    console.log('All tasks complete')
 }
 
 buildLib();
