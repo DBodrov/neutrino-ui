e7b84ea06a820a78213966703a25666b3004d353
D.Bodrov <dmbodrov@hotmail.com>
2020-04-03 23:47:31 +0300

    fix InputPage

--- site/examples/InputPage.tsx
+++ site/examples/InputPage.tsx
@@ -1,17 +1,29 @@
 import React, { useState, createRef } from 'react';
-import styled from '@emotion/styled'
-import { Input, Span, Button } from 'neutrino-ui';
+import styled from '@emotion/styled';
+import { ThemeProvider } from 'emotion-theming';
+import { Input, Span, Button, createTheme } from 'neutrino-ui';
 
 const Label = styled(Span)`
     display: block;
     margin-bottom: 10px;
 `;
 
+const myTheme = createTheme({
+    colors: {
+        mainColors: {
+            primary: 'green'
+        }
+    }
+});
+
+const outerInputStyle: React.CSSProperties = {
+    borderRadius: '10px'
+}
+
 export function InputPage() {
     const [value, setValue] = useState('');
     const inputRef = createRef<HTMLInputElement>();
     const setFocus = () => inputRef?.current?.focus();
-
 
     return (
         <section css={{ padding: '1rem', display: 'flex', flexFlow: 'column nowrap', height: '100%' }}>
@@ -19,32 +31,33 @@ export function InputPage() {
                 <Label>Type: text</Label>
                 <Input name="text" onChangeHandler={(v, e) => setValue(v)} value={value} />
                 <div css={{ marginTop: '1rem' }}>
-                    <Label>Add CSS props</Label>
-                    <Input
-                        name="text"
-                        onChangeHandler={(v, e) => setValue(v)}
-                        value={value}
-                        css={{ color: 'red', borderRadius: '10px', ':hover': {borderColor: 'green'} }}
-                    />
+                    <ThemeProvider theme={myTheme}>
+                        <Label>Change look with CSS and Style props</Label>
+                        <Input
+                            name="text"
+                            onChangeHandler={(v, e) => setValue(v)}
+                            value={value}
+                            css={{ color: 'red' }}
+                            style={outerInputStyle}
+                            autoComplete="off"
+                        />
+                    </ThemeProvider>
                 </div>
                 <div css={{ marginTop: '1rem' }}>
                     <Label>hasError = true</Label>
-                    <Input
-                        name="text"
-                        onChangeHandler={(v, e) => setValue(v)}
-                        value={value}
-                        hasError
-                    />
+                    <Input name="text" onChangeHandler={(v, e) => setValue(v)} value={value} hasError />
                 </div>
-                <div css={{ marginTop: '1rem', display: 'flex', flexFlow: 'column nowrap', justifyContent: 'space-around', height: '150px'}}>
+                <div
+                    css={{
+                        marginTop: '1rem',
+                        display: 'flex',
+                        flexFlow: 'column nowrap',
+                        justifyContent: 'space-around',
+                        height: '150px',
+                    }}>
                     <Label>Set focus</Label>
                     <Button onClick={setFocus}>Set focus</Button>
-                    <Input
-                        name="text"
-                        ref={inputRef}
-                        onChangeHandler={(v, e) => setValue(v)}
-                        value={value}
-                    />
+                    <Input name="text" ref={inputRef} onChangeHandler={(v, e) => setValue(v)} value={value} />
                 </div>
             </div>
         </section>
