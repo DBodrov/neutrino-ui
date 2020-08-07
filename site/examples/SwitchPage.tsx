3fa58920257d46d20207c777ff784c327931a54d
D.Bodrov <D.Bodrov@otpbank.ru>
2020-08-08 00:34:39 +0300

    add common config add Fast refresh improve docs

--- site/examples/SwitchPage.tsx
+++ site/examples/SwitchPage.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 import { css } from '@emotion/core';
 
-import { Switch, createTheme, ISwitchProps } from 'neutrino-ui';
+import { Switch } from 'neutrino-ui';
 
 import { Example, Wrapper } from './Example';
 
-const onBackground = 'linear-gradient(0deg, rgba(25, 145, 235, 0.4) 2.22%, rgba(45, 161, 248, 0.4) 98.44%);'
-const offBackground = '#222C3C';
-
+const onBackground = 'green';
+const offBackground = '#cacaca';
 
 const buttonCss = css`
   width: 16px;
@@ -16,7 +15,6 @@ const buttonCss = css`
 `;
 
 export function SwitchPage() {
-
   const [on, setOn] = useState(false);
   const trackCss = css`
     width: 48px;
@@ -27,7 +25,53 @@ export function SwitchPage() {
 
   return (
     <Wrapper>
-      <Switch on={on} onToggle={() => setOn(on => !on)} trackCss={trackCss} buttonCss={buttonCss}/>
+      <Example code={exampleDefault} />
+      <Switch
+        on={on}
+        onToggle={() => setOn((on) => !on)}
+        trackCss={trackCss}
+        buttonCss={buttonCss}
+        disabled={false}
+      />
     </Wrapper>
-  )
+  );
 }
+
+const exampleDefault = `
+import React, { useState } from 'react';
+import { css } from '@emotion/core';
+import { Switch } from 'neutrino-ui';
+
+const onBackground = '#008000';
+const offBackground = '#cacaca';
+
+const buttonCss = css({
+  width: '16px',
+  height: '16px'
+  backgroundColor: '#fff';
+
+})
+';
+
+export function SwitchPage() {
+  const [on, setOn] = useState(false);
+  const trackCss = css({
+    width: '48px'
+    height: '24px'
+    background: on ? onBackground : offBackground,
+    padding: '4px'
+  });
+
+  return (
+    <>
+      <Switch
+        on={on}
+        onToggle={() => setOn((on) => !on)}
+        trackCss={trackCss}
+        buttonCss={buttonCss}
+        disabled
+      />
+    </>
+  );
+}
+`.trim();
