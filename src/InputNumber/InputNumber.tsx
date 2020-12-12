84fd58676e3841e436f35b08c8d9d12667de6ecf
D.Bodrov <D.Bodrov@otpbank.ru>
2020-12-12 12:34:42 +0300

    microedit input number

--- src/InputNumber/InputNumber.tsx
+++ src/InputNumber/InputNumber.tsx
@@ -1,109 +1,107 @@
-import React, { useState } from 'react';
-import { css } from '@emotion/react'
-import { isEmptyString } from '../utils/string.utils';
-import { toDecimalString } from '../utils/numeric.utils';
-import { Input } from '../Input';
-import { IInputNumberProps } from './types';
+import React, {useState} from 'react';
+import {css} from '@emotion/react';
+import {isEmptyString} from '../utils/string.utils';
+import {toDecimalString} from '../utils/numeric.utils';
+import {Input} from '../Input';
+import {IInputNumberProps} from './types';
 
 const toString = (value: string | number) => {
-    if (typeof value === 'number') {
-        return String(value);
-    }
-    return value;
+  if (typeof value === 'number') {
+    return String(value);
+  }
+  return value;
 };
 
 const disableSpinBtn = css({
-    '&::-webkit-inner-spin-button, &::-webkit-outer-spin-button': { appearance: 'none', margin: 0 },
-    appearance: 'textfield',
+  '&::-webkit-inner-spin-button, &::-webkit-outer-spin-button': {appearance: 'none', margin: 0},
+  appearance: 'textfield',
 });
 
 export function InputNumber(props: IInputNumberProps) {
-    const {
-        type,
-        hasError = false,
-        disabled = false,
-        locales = 'ru-RU',
-        formatOptions = {},
-        styles = {},
-        value = '',
-        parser = 'parseFloat',
-        zeroWhenEmpty = false,
-        onFocusHandler,
-        onChangeHandler,
-        onBlurHandler,
-        name,
-        ...inputProps
-    } = props;
+  const {
+    type,
+    disabled = false,
+    locales = 'ru-RU',
+    formatOptions = {},
+    value = '',
+    parser = 'parseFloat',
+    zeroWhenEmpty = false,
+    onFocusHandler,
+    onChangeHandler,
+    onBlurHandler,
+    name,
+    ...inputProps
+  } = props;
 
-    const [isEditing, setIsEditing] = useState(false);
-    const hasFormatOptions = Object.keys(formatOptions).length > 0;
+  const [isEditing, setIsEditing] = useState(false);
+  const hasFormatOptions = Object.keys(formatOptions).length > 0;
 
-    const applyNumberOptions = (value: string) => {
-        if (isEmptyString(value)) return '';
-        const result = Number(value);
-        const fractionDigits = formatOptions.maximumFractionDigits || 3;
-        return result.toFixed(fractionDigits);
-    };
+  const applyNumberOptions = (value: string) => {
+    if (isEmptyString(value)) return '';
+    const result = Number(value);
+    const fractionDigits = formatOptions.maximumFractionDigits || 3;
+    return result.toFixed(fractionDigits);
+  };
 
-    const formatAsLocaleString = (rawValue: string | number) => {
-        const valueStringified = toString(rawValue);
-        return toDecimalString(valueStringified, locales, formatOptions, parser);
-    };
+  const formatAsLocaleString = (rawValue: string | number) => {
+    const valueStringified = toString(rawValue);
+    return toDecimalString(valueStringified, locales, formatOptions, parser);
+  };
 
-    const formatAsNumber = (value: string | number) => {
-        const valueStringified = toString(value);
-        const numericVal = hasFormatOptions ? applyNumberOptions(valueStringified) : valueStringified;
+  const formatAsNumber = (value: string | number) => {
+    const valueStringified = toString(value);
+    const numericVal = hasFormatOptions ? applyNumberOptions(valueStringified) : valueStringified;
 
-        if (zeroWhenEmpty) {
-            return isEmptyString(numericVal) ? 0 : Number[parser](numericVal);
-        }
-        return isEmptyString(numericVal) ? '' : Number[parser](numericVal);
-    };
+    if (zeroWhenEmpty) {
+      return isEmptyString(numericVal) ? 0 : Number[parser](numericVal);
+    }
+    return isEmptyString(numericVal) ? '' : Number[parser](numericVal);
+  };
 
-    const handleChange = (value: string, event: React.ChangeEvent<HTMLInputElement>) => {
-        const formattedValue = formatAsNumber(value);
-        onChangeHandler && onChangeHandler(formattedValue, event);
-    };
+  const handleChange = (value: string, event: React.ChangeEvent<HTMLInputElement>) => {
+    const formattedValue = formatAsNumber(value);
+    onChangeHandler && onChangeHandler(formattedValue, event);
+  };
 
-    const handleFocus: React.FocusEventHandler<HTMLInputElement> = (event) => {
-        setIsEditing(true);
-        const val = Number(event.target.value);
-        onFocusHandler && onFocusHandler(val, event);
-    };
+  const handleFocus: React.FocusEventHandler<HTMLInputElement> = event => {
+    setIsEditing(true);
+    const val = Number(event.target.value);
+    onFocusHandler && onFocusHandler(val, event);
+  };
 
-    const handleBlur: React.FocusEventHandler<HTMLInputElement> = (event) => {
-        setIsEditing(false);
-        const val = Number(event.target.value);
-        onBlurHandler && onBlurHandler(val, event);
-    };
+  const handleBlur: React.FocusEventHandler<HTMLInputElement> = event => {
+    setIsEditing(false);
+    const val = Number(event.target.value);
+    onBlurHandler && onBlurHandler(val, event);
+  };
 
-    return (
-        <>
-            {isEditing ? (
-                <Input
-                    css={disableSpinBtn}
-                    value={value}
-                    name={name}
-                    type="number"
-                    onBlur={handleBlur}
-                    onChangeHandler={handleChange}
-                    disabled={disabled}
-                    autoComplete="off"
-                    {...inputProps}
-                />
-            ) : (
-                <Input
-                    css={disableSpinBtn}
-                    value={formatAsLocaleString(value)}
-                    name={name}
-                    type="tel"
-                    onFocus={handleFocus}
-                    onChangeHandler={undefined}
-                    disabled={disabled}
-                    autoComplete="off"
-                    {...inputProps}
-                />
-            )}
-        </>
-    );
+  return (
+    <>
+      {isEditing ? (
+        <Input
+          css={disableSpinBtn}
+          value={value}
+          name={name}
+          type="number"
+          onBlur={handleBlur}
+          onChangeHandler={handleChange}
+          disabled={disabled}
+          autoComplete="off"
+          {...inputProps}
+        />
+      ) : (
+        <Input
+          css={disableSpinBtn}
+          value={formatAsLocaleString(value)}
+          name={name}
+          type="tel"
+          onFocus={handleFocus}
+          onChangeHandler={undefined}
+          disabled={disabled}
+          autoComplete="off"
+          {...inputProps}
+        />
+      )}
+    </>
+  );
 }
