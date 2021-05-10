451d1f3ba926e2cdeb87b5f2e05e75e467f8994b
DBodrov <dmbodrov@hotmail.com>
2021-05-10 19:53:22 +0300

    cfg

--- scripts/buildDocs.js
+++ scripts/buildDocs.js
@@ -2,17 +2,19 @@ const {buildSync} = require('esbuild');
 const fs = require('fs-extra');
 
 function docsBuild() {
-  fs.rmSync('../docs', {recursive: true});
+  fs.rmSync('docs', {recursive: true, force: true});
   buildSync({
-    entryPoints: ['../site/index.tsx'],
+    entryPoints: ['./site/index.tsx'],
     bundle: true,
     minify: true,
     sourcemap: true,
-    target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
-    outdir: '../docs',
+    target: ['chrome80', 'firefox80', 'safari12'],
+    outdir: './docs',
     loader: {'.svg': 'dataurl', '.png': 'dataurl'},
     define: {
       'process.env.NODE_ENV': 'production',
     },
   });
 }
+
+docsBuild();
